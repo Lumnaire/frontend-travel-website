b654ae20b48856c556e3b982faba72fb3693d78e
Ronald Castromero <callme14noy@gmail.com>
2025-07-24 22:22:31 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './index.css'
+
+function App() {
+
+
+  return (
+    <>
+   <h1 className=' bg-blue-500'>Welcome to Lumnaire Collaborative Development Project</h1>
+    
+    </>
+  )
+}
+
+export default App
