691617ab3284f5c85128de3f18906ad77adc35b5
Yna <ynacastillo896@gmail.com>
2025-07-25 17:06:47 +0800

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,33 @@
 import { Routes, Route, useLocation } from "react-router-dom";
+//importing react slick slider
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { animateScroll } from "react-scroll";
-import { useEffect } from "react";
 
-import NavBar from "./components/organs/NavBar";
+import { animateScroll } from "react-scroll";
+
+import NavBar from "./components/organs/NavBar"
 import Home from "./components/pages/Home";
+import { useEffect } from "react";
 import Footer from "./components/organs/Footer";
 
 function App() {
-  const location = useLocation();
-
+  const directory = useLocation();
   useEffect(() => {
-    animateScroll.scrollToTop({ duration: 0 });
-  }, [location.pathname]);
+    animateScroll.scrollToTop({
+      duration: 0,
+    });
+  }, [directory.pathname]);
 
   return (
     <div className="w-full bg-white text-gray-950 font-poppins">
-      <p>Welcome to Lumnaire Collaborative Development Project</p>
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
       <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
