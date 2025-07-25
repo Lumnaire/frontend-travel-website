7a68ddd8975dda23f9cb8157efcfdeaf15fc6641
Yna <ynacastillo896@gmail.com>
2025-07-25 17:15:24 +0800

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
 import { Routes, Route, useLocation } from "react-router-dom";
-//importing react slick slider
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
 import { animateScroll } from "react-scroll";
-
-import NavBar from "./components/organs/NavBar"
+import NavBar from "./components/organs/NavBar";
 import Home from "./components/pages/Home";
 import { useEffect } from "react";
 import Footer from "./components/organs/Footer";
 
 function App() {
-  const directory = useLocation();
+  const location = useLocation();
+
   useEffect(() => {
-    animateScroll.scrollToTop({
-      duration: 0,
-    });
-  }, [directory.pathname]);
+    animateScroll.scrollToTop({ duration: 0 });
+  }, [location.pathname]);
 
   return (
     <div className="w-full bg-white text-gray-950 font-poppins">
@@ -26,8 +22,8 @@ function App() {
       </Routes>
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
 
